e40278435acd0eb48df21f9a685ecee2a0a7ea1e
Ron Ammar <rammar@Kermito.local>
2015-12-23 16:20:42 -0500

    Course project code and tidy output file.

---
+++ Codebook.md
@@ -0,0 +1,42 @@
+Here we analyze smartphone-based movement as measured in a [2012 study](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
+
+Raw source data (available [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)) are based on subjects who were wearing accelerometer- and gyroscope- embedded Samsung smartphones.
+
+Subjects could engage in 6 activities:
+
+1. WALKING
+2. WALKING_UPSTAIRS
+3. WALKING_DOWNSTAIRS
+4. SITTING
+5. STANDING
+6. LAYING
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ.
+
+3-axial signals in the X, Y and Z directions were annotated as:
+
+tBodyAcc-XYZ
+tGravityAcc-XYZ
+tBodyAccJerk-XYZ
+tBodyGyro-XYZ
+tBodyGyroJerk-XYZ
+tBodyAccMag
+tGravityAccMag
+tBodyAccJerkMag
+tBodyGyroMag
+tBodyGyroJerkMag
+fBodyAcc-XYZ
+fBodyAccJerk-XYZ
+fBodyGyro-XYZ
+fBodyAccMag
+fBodyAccJerkMag
+fBodyGyroMag
+fBodyGyroJerkMag
+
+The set of variables that were estimated from these signals are: 
+
+mean(): Mean value
+std(): Standard deviation
+
+The complete list of variables of each feature vector is available in 'features.txt'
+from the source data.
---
+++ run_analysis.R
@@ -0,0 +1,119 @@
+##### Coursera course project for 'Getting and Cleaning Data'
+# Date: Dec/23/2015
+# Author: Ron Ammar
+
+# NOTE: At the outset, the objectives of this course project were not clear.
+# From what I understand, we are to examine only 3 files within /train and 
+# /test of the UCI HAR dataset, skipping the subfolders and their respective
+# testing and training data. I would recommend improving the assignment handout
+# to be more descriptive.
+
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+
+setwd("~/learning/coursera/3 - getting and cleaning data/UCI HAR Dataset/")
+
+
+mergeLabelandExtract <- function() {
+  # 1) Merges the training and test sets to create one data set.
+  # 3) Uses descriptive activity names to name the activities in the data set.
+  # 4) Labels the variable names descriptively (simpler to do this upon
+  #    data import, rather than after steps 2 and 3).
+  # 2) Extracts the mean and standard deviation for each measurement.
+  #
+  # Returns:
+  #   A master list integrating subject ID, type of activity and all
+  #   measurements for both training and testing data.
+  
+  dir.create("merged")
+  
+  # Get all files with the "test" or "train" suffix
+  # We're not interested in the subfolders, so no recursive listing.
+  files <- list.files("test", recursive=F)
+  testingFiles <- paste("test/", files[str_detect(files, ".+test\\.txt")],
+                        sep="")
+  files <- list.files("train", recursive=F)
+  trainingFiles <- paste("train/", files[str_detect(files, ".+train\\.txt")],
+                         sep="")
+  
+  # Here, I'm making the assumption (based on prior knowledge) that the file
+  # number and order in both test and train sets is the same and both have
+  # the same prefixes at the same point in the list. We could run more checks
+  # with regex in a production version of this script, but the true intention
+  # is to only run this analysis once.
+  for (i in 1:length(testingFiles)) {
+    # Merge training and testing data into a single data set.
+    # NOTE: sep="" uses any whitespace, logically equivlent to "\s+"
+    te <- read.delim(testingFiles[i], header=F, sep="", colClasses="numeric")
+    tr <- read.delim(trainingFiles[i], header=F, sep="", colClasses="numeric")
+    merged <- bind_rows(te, tr)
+    
+    # Extract leading directory names
+    filePrefix <- str_match(testingFiles[i], "([^/]+)test\\.txt")[1, 2]
+    write.table(merged, paste("merged/", filePrefix, "merged.txt", sep=""),
+                row.names=F, col.names=F)
+  }
+  
+  # Create a single data frame combining the 3 sources of data which have now
+  # been merged.
+  
+  # Import activities and use activity labels
+  activity <- read.delim("merged/y_merged.txt", col.names="activity",
+                         header=F, sep="", colClasses="numeric")
+  activityLabels <- read.delim("activity_labels.txt",
+                               col.names=c("activity", "ActivityLabels"),
+                               header=F, sep="")
+  activity <- left_join(activity, activityLabels, by="activity")
+  activity <- select(activity, -activity)  # no need for numerical column
+  
+  
+  subjectID <- read.delim("merged/subject_merged.txt", 
+                          col.names="SubjectID", header=F, sep="",
+                          colClasses="numeric")
+  
+  # Import features and use them as the header for measurements
+  features <- (read.delim("features.txt", header=F, sep="",
+                         colClasses="character"))[, 2]  # extract 2nd column
+  measurements <- read.delim("merged/X_merged.txt", col.names=features,
+                             header=F, sep="", colClasses="numeric")
+  
+  # Only include mean or stdev features.
+  meanOrStdevFeatureIndeces <- str_detect(features, "(mean|std)\\(\\)")
+  measurements <- measurements[, meanOrStdevFeatureIndeces]
+  
+  master <- bind_cols(subjectID, activity, measurements)
+  
+  return(master)
+}
+
+
+computeMeans <- function(master) {
+  # 5) Creates a second independent tidy data set with the average of each
+  # variable for each activity and each subject.
+  #
+  # Output:
+  #   Creates new file of table for this analysis.
+  
+  means <- master %>%
+    group_by(SubjectID, ActivityLabels) %>%
+    summarise_each(funs(mean)) %>%
+    gather("measurement", "mean", -c(SubjectID, ActivityLabels))
+  
+  write.table(means, "means_tidy.txt", sep="\t", row.names=F)
+}
+
+
+# Run the functions above.
+m <- mergeLabelandExtract()
+computeMeans(m)
+
+
+
+
+
+
+
+
+
